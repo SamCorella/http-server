13c71e1da8b92a22da8e838ff1ed10076629e3da
Sam Corella <samcorella77@gmail.com>
2024-11-23 12:46:18 -0500

    Refactor and add comments

--- server.c
+++ server.c
@@ -2,57 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netdb.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define BACKLOG 10
 
-int main() {
-    int status;
-    int sfd, new_fd;
-    socklen_t addr_size;
-    struct addrinfo hints;
-    struct addrinfo *res, *rp;
+int main(void) {
+    int sockfd, new_fd;
+    socklen_t addrlen;
+    struct addrinfo hints, *res, *rp;
     struct sockaddr_storage client_addr;
 
-    char *msg = "Yeehaw it works\n";
+    const char *msg = "Yeehaw it works\n";
     int len, bytes_sent;
 
+    // Get address to bind to
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if ((status = getaddrinfo(NULL, "3490", &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+    if (getaddrinfo(NULL, "3490", &hints, &res) != 0) {
+        perror("getaddrinfo");
+        exit(EXIT_FAILURE);
     }
 
+    // Try to bind to each address until we succeed
     for (rp = res; rp != NULL; rp = rp->ai_next) {
-        sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 
-        if (sfd == -1)
+        if (sockfd == -1)
             continue;
-        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+        if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0)
             break;
 
-        close(sfd);
+        close(sockfd);
     }
 
     freeaddrinfo(res);
 
+    // If we couldn't bind, exit
     if (rp == NULL) {
-        fprintf(stderr, "Could not bind\n");
-        exit(1);
+        perror("Could not bind");
+        exit(EXIT_FAILURE);
     }
 
-    listen(sfd, BACKLOG);
+    listen(sockfd, BACKLOG);
 
-    addr_size = sizeof client_addr;
-    new_fd = accept(sfd, (struct sockaddr *)&client_addr, &addr_size);
+    // Accept a connection
+    addrlen = sizeof client_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&client_addr, &addrlen);
 
+    // Send a message back to the client
     len = strlen(msg);
     bytes_sent = send(new_fd, msg, len, 0);
+
     close(new_fd);
 }
